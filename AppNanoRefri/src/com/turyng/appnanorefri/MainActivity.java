7b732fbc137f50d54a434c51a957200d5efd9457
Kelvyn <Kelvynjpereira@hotmail.com>
2019-08-25 21:15:40 -0300

    Update commit
    Verificar linha 35 da classe ValidacaoPagamento, esta dando uma excption
    
    Co-Authored-By: murilo27 <murilo27@users.noreply.github.com>

---
+++ AppNanoRefri/src/com/turyng/appnanorefri/MainActivity.java
@@ -0,0 +1,51 @@
+package com.turyng.appnanorefri;
+
+import com.example.appnanorefri.R;
+import com.turyng.appnanorefri.validacao.ValidacaoPagamento;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);	
+		
+		// ======================================================
+		
+		/*
+		 * Instancia e chamada do metodo de pagamento
+		 */
+		
+		ValidacaoPagamento validar = new ValidacaoPagamento();
+		validar.validar_pagamento();
+		
+		
+		// ======================================================
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
