49017e426a7e0d7311dcfb636ec12c0ffb4786d8
Kelvyn <Kelvynjpereira@hotmail.com>
2019-08-26 00:28:49 -0300

    Update commit

--- AppNanoRefri/src/com/turyng/appnanorefri/MainActivity.java
+++ AppNanoRefri/src/com/turyng/appnanorefri/MainActivity.java
@@ -1,51 +1,69 @@
 package com.turyng.appnanorefri;
 
-import com.example.appnanorefri.R;
-import com.turyng.appnanorefri.validacao.ValidacaoPagamento;
+import java.io.IOException;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.util.Log;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);	
+		setContentView(R.layout.activity_main);
 		
-		// ======================================================
-		
-		/*
-		 * Instancia e chamada do metodo de pagamento
-		 */
-		
-		ValidacaoPagamento validar = new ValidacaoPagamento();
-		validar.validar_pagamento();
-		
-		
-		// ======================================================
-		
-	}
+		// Instancia da classe de requisicao
+        OkHttpClient client = new OkHttpClient();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // Requisicao
+       String url = "https://snapy.io/api/v1/send";
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+       RequestBody formBody = new FormBody.Builder()
+                .add("to", "xrb_33kuw5z8jdsx8bssnnzom1oj1hdejuwsb6adbj4b8u9tkuat4a7a1bxg4efs")
+                .add("from", "xrb_3tg3c1pjkzy1s7bgep4rjw7c5gb7rwi15mt9my6ioy8ow4sj3jnsh4t9yykt")
+                .add("amount", "1")
+                .add("password", "123456789teste")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .header("x-api-key", "pub_5cf8389da5376d365a0a325d-b840790d-50b5-49b0-a8dc-7f7ce9863a9f")
+                .post(formBody)
+                .build();
+
+       client.newCall(request).enqueue(new Callback() {
+    	   
+    	@Override
+   		public void onFailure(Call call, IOException e) {
+   			// TODO Auto-generated method stub
+    		 e.printStackTrace();
+   		}
+		
+		@Override
+		public void onResponse(Call call, Response response) throws IOException {
+			// TODO Auto-generated method stub
+			if(response.isSuccessful()){
+                final String myResponse = response.body().string();
+                Log.i("TESTE", myResponse);
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("CARTEIRA", myResponse);
+                    }
+                });
+            }
+			
 		}
-		return super.onOptionsItemSelected(item);
+		
+       });
 	}
 }
