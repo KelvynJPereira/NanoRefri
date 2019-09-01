471e2c25aa7a8c275d2b2409a020a2fa78cb1901
Kelvyn <Kelvynjpereira@hotmail.com>
2019-09-01 19:02:37 -0300

    Implementation commit
    Add new activitys

--- NanoRefriApp/gen/com/example/nanorefri/R.java
+++ NanoRefriApp/gen/com/example/nanorefri/R.java
@@ -22,12 +22,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int qr_code=0x7f020001;
+        public static final int coke=0x7f020000;
+        public static final int fanta=0x7f020001;
+        public static final int guarana=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int qr_code=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int imageView1=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int imageButton1=0x7f080001;
+        public static final int imageButton2=0x7f080002;
+        public static final int imageButton3=0x7f080003;
+        public static final int imageView1=0x7f080004;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
---
+++ NanoRefriApp/src/com/nanorefri/AmountActivity.java
@@ -0,0 +1,16 @@
+package com.nanorefri;
+
+import com.example.nanorefri.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AmountActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_amount);
+	
+	}
+}
